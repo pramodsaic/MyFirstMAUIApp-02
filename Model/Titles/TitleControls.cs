6d426781c84be024bbe0d56f1ec334145e4b611d
pramodsaic <chpramodsai@gmail.com>
2026-01-08 14:34:28 -0600

    .

---
+++ Model/Titles/TitleControls.cs
@@ -0,0 +1,12 @@
+﻿namespace MyFirstMAUIApp.Model
+{
+    public static class TitleControls
+    {
+        public static string Title { get; set; } = "Contro;s Menu";
+        public static string ButtonTextSlider { get; set; } = "Collection";
+        public static string ButtonTextCollectionImages { get; set; } = "Slider Page";
+        public static string ButtonTextStepper { get; set; } = "Stepper Page";
+        public static string ButtonTextSwitch { get; set; } = "Switch";
+
+    }
+}
