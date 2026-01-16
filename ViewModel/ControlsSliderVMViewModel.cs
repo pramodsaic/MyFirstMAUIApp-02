e3e10816804497555aa3384bc604bffe35868ba3
pramodsaic <chpramodsai@gmail.com>
2026-01-16 14:25:53 -0600

    .

--- ViewModel/ControlsSliderVMViewModel.cs
+++ ViewModel/ControlsSliderVMViewModel.cs
@@ -1,11 +1,6 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
 using MyFirstMAUIApp.Model.Titles;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace MyFirstMAUIAPP.ViewModel
 {
